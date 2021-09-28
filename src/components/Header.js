a8671a8007fda55ffd50d81f0c7a2bd7aa7f6a0f
Daniela <martinezdl_@hotmail.com>
2021-09-27 22:24:33 -0300

    change profile img

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@
 import React from 'react';
 import { Stack, Flex, Circle, Box, Text, Button, Image, Spacer } from '@chakra-ui/react';
 import { useMediaQuery } from '@chakra-ui/media-query';
+
+import profilePhoto from '../assets/profilePic.jpg'
+
 export const Header = () => {
-
     const [isNotSmallerScreen] = useMediaQuery("(min-width:800px)");
-
 
     return (
         <Stack>
-            <Circle position="absolute" bg="blue.100" opacity="0.1" w="300px" h="300px" alignSelf="flex-end" />
+            <Circle position="absolute" bg="blue.100" opacity="0.1" w="200px" h="200px" alignSelf="flex-end" />
             <Flex direction={isNotSmallerScreen ? "row" : "column"}
                 spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
                 alignSelf="flex-start">
@@ -19,10 +20,13 @@ export const Header = () => {
                         bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text'>Dani
                     </Text>
                     <Text> HTML-Css-JavaScript-React </Text>
-                    <Button mt="8" colorScheme="blue" onClick={() => window.open("https://github.com/DanMartinez01")}>Hire me</Button>
+                    <Button mt="8" colorScheme="blue"
+                        onClick={() => window.open("https://github.com/DanMartinez01")}>
+                        Hire me
+                    </Button>
                 </Box>
                 <Image mt={isNotSmallerScreen ? "0" : "12"} mb={isNotSmallerScreen ? "0" : "12"}
-                    src="https://github.com/github.png?size=460" borderRadius="full" alignSelf="center" boxSize="300px"
+                    src={profilePhoto} borderRadius="full" alignSelf="center" boxSize="200px"
                     backgroundColor="transparent" boxShadow="lg"
                 />
             </Flex>
